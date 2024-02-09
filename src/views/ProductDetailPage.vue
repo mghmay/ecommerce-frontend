48bd4289ff699536bcbe8cb0645de4282b70a288
Mathew May <29102292+mghmay@users.noreply.github.com>
2024-02-09 15:32:18 +0000

    Add product detail page template and logic

--- src/views/ProductDetailPage.vue
+++ src/views/ProductDetailPage.vue
@@ -1,11 +1,27 @@
 <template>
-  <div>
-    <h1>This is the Product detail page</h1>
+  <div id="page-wrap">
+    <div id="img-wrap">
+      <img :src="product.imageUrl" />
+    </div>
+    <div id="product-details">
+      <h1>{{ product.name }}</h1>
+      <h3 id="price">${{ product.price }}</h3>
+      <p>Average rating: {{ product.averageRating }}</p>
+      <button id="add-to-cart">Add to cart</button>
+      <h4>Description</h4>
+      <p>{{ product.description }}</p>
+    </div>
   </div>
 </template>
 <script>
+import { products } from '@/fake-data'
 export default {
-  name: 'ProductDetailPage'
+  name: 'ProductDetailPage',
+  data() {
+    return {
+      product: products.find((p) => p.id === this.$route.params.id)
+    }
+  }
 }
 </script>
 
