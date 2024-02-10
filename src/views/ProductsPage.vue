54e84ed6224acdc9d5b1fc3c778930af35642f37
Mathew May <29102292+mghmay@users.noreply.github.com>
2024-02-10 17:04:16 +0000

    Refactor products page

--- src/views/ProductsPage.vue
+++ src/views/ProductsPage.vue
@@ -1,18 +1,10 @@
 <template>
   <div id="page-wrap">
-    <div class="grid-wrap">
-      <div v-for="product in products" :key="product.id" class="product-item">
-        <img :src="product.imageUrl" />
-        <h3>{{ product.name }}</h3>
-        <p class="product-price">${{ product.price }}</p>
-        <router-link :to="'/products/' + product.id">
-          <button>View Details</button>
-        </router-link>
-      </div>
-    </div>
+    <ProductsGrid :products="products" />
   </div>
 </template>
 <script>
+import ProductsGrid from '@/components/ProductsGrid.vue'
 import { products } from '@/fake-data'
 
 export default {
@@ -21,43 +13,9 @@ export default {
     return {
       products
     }
+  },
+  components: {
+    ProductsGrid
   }
 }
 </script>
-<style scoped>
-.grid-wrap {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  margin-top: 16px;
-}
-
-.product-item {
-  align-items: center;
-  border-radius: 8px;
-  box-shadow: 0px 2px 5px #888;
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 2%;
-  padding: 20px;
-  position: relative;
-  width: 32%;
-}
-
-.product-name {
-  margin-bottom: 0;
-}
-
-img {
-  height: 200px;
-  width: 200px;
-}
-
-a {
-  width: 100%;
-}
-
-button {
-  width: 100%;
-}
-</style>
