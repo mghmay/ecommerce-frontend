c069911cc8617141ea1233b2902907db5a6efcdd
Mathew May <29102292+mghmay@users.noreply.github.com>
2024-02-18 20:40:26 +0000

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8000',
+      '/images': 'http://localhost:8000'
     }
   }
 })
